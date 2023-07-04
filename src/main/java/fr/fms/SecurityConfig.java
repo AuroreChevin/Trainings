068f677104e1fceb8c47574d49bd4327cf54e8ac
Aurore Chevin <chevinaurore@gmail.com>
2023-07-04 10:41:47 +0200

    JwtAuthenticationFilter OK

--- src/main/java/fr/fms/SecurityConfig.java
+++ src/main/java/fr/fms/SecurityConfig.java
@@ -5,6 +5,7 @@ import fr.fms.service.AccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,8 +16,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -25,20 +24,30 @@ import java.util.Collection;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
 
-    @Autowired
+    /*@Autowired
     private UserDetailsService userDetailsService;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }*/
     @Autowired
     private AccountServiceImpl accountService;
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        //http.formLogin();
+       // http.formLogin();
         http.authorizeHttpRequests().anyRequest().authenticated();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JwtAuthenticationFilter(authenticationManagerBean()));
     }
-    @Autowired (required = true)
-    DataSource dataSource;
+    //@Autowired (required = true)
+    //DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService((new UserDetailsService() {
@@ -51,13 +60,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 });
                 return new User(user.getUsername(), user.getPassword(), authorities);
             }
-        }));
+        }));}
        /* auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("select username as principal, password as credentials, active from T_Users where username=?")
                 .authoritiesByUsernameQuery("select username as principal, role as role from T_Users_Roles where username=?")
                 .rolePrefix("ROLE_")
                 .passwordEncoder(passwordEncoder());*/
-    }
+
    /* @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
